ec86d1dca45d99646ba074bc7baf56c64bb39d0c
Roelof Anne <roelofanne@gmail.com>
2018-08-01 21:16:55 +0200

    Add files via upload

---
+++ ProgrammerMulti/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using ReProBuild;
+//Using Tag
+
+namespace Application
+{
+    public class AppRe
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder fixedStart = new StringBuilder();
+            fixedStart.Append("using System;");
+            fixedStart.Append("namespace newworld");
+            fixedStart.Append("{");
+            fixedStart.Append(" class MainNew");
+            fixedStart.Append(" {");
+            fixedStart.Append("    public static void Main(string[] args)");
+            fixedStart.Append("    {");
+            string[] midpart = new string[] {
+                "Console.WriteLine(\"Hello new world);",
+                "Console.WriteLine(\"Hello new world\");",
+                "Console.WriteLie(\"Hello new world\");",
+                "Console.WriteLine(\"Hello new world\");Console.WriteLine(\"Hello new world\");"};
+ 
+            StringBuilder fixedEnd = new StringBuilder();
+            fixedEnd.Append("    }");
+            fixedEnd.Append(" }");
+            fixedEnd.Append("}");
+
+            for (int i = 0; i < 4; i++)
+            {
+				Console.WriteLine(CompileDecompiles.CompileExecutable("test" + i.ToString(), fixedStart.ToString() + midpart[i] + fixedEnd.ToString()));
+            }
+        }
+    }
+}
